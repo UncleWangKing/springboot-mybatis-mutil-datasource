cecbec51fcf3e28844b3dc3b973d4d709eeb3899
l05 <285296372@qq.com>
2018-06-29 15:06:59 +0800

    ZY_XB表

--- src/main/java/org/spring/springboot/domain/XiaoBan.java
+++ src/main/java/org/spring/springboot/domain/XiaoBan.java
@@ -2,6 +2,9 @@ package org.spring.springboot.domain;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 /**
  * @author ZhangDaPang 285296372@qq.com
  * @date 2018/6/29 11:25
@@ -10,4 +13,55 @@ import lombok.Data;
 public class XiaoBan {
     private Long id;
     private String shape;
+    private BigDecimal area;
+    private BigDecimal perimeter;
+    private Integer xbln;
+    private Integer xblnId;
+    private Integer xianju;
+    private Integer xiangzhenchang;
+    private Integer xingzhengcun;
+    private Integer linbanhao;
+    private Integer xiaobanhao;
+    private Integer suoshuxian;
+    private Integer suoshuxiangzhen;
+    private String keyWord;
+    private BigDecimal mianji;
+    private String tufuhao;
+    private String guidaohao;
+    private Date jieshoushijian;
+    private Integer senlinleibie;
+    private Integer dilei;
+    private Integer lindiquanshu;
+    private Integer linmuquanshu;
+    private Integer projectType;
+    private Integer linzhong;
+    private Integer shiquandengji;
+    private Integer baohudengji;
+    private Integer dimao;
+    private Integer podu;
+    private Integer powei;
+    private Integer poxiang;
+    private Integer qiyuan;
+    private Integer youshishuzhong;
+    private Integer lingzu;
+    private BigDecimal yubidu;
+    private Integer jingjilinchanqi;
+    private Integer meigongqingzhushu;
+    private BigDecimal meigongqingchuji;
+    private BigDecimal xiaobanchujiliang;
+    private BigDecimal sipangshuzhushu;
+    private BigDecimal sipangshuchuji;
+    private BigDecimal lvhuahzushu;
+    private BigDecimal lvhuachuji;
+    private BigDecimal sanshengmuzhushu;
+    private BigDecimal sanshengmuchuji;
+    private BigDecimal sanshengzhuzhushu;
+    private Integer shidi;
+    private Integer huangmoleixing;
+    private Integer huangmochengdu;
+    private Integer shahualeixing;
+    private Integer shahuachengdu;
+    private Integer shididengji;
+    private BigDecimal pointX;
+    private BigDecimal pointY;
 }
