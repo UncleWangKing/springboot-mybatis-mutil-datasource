2fc32f20cf5d644bbb89679f7792af5656ff27e4
l05 <285296372@qq.com>
2018-07-10 16:21:25 +0800

    替换全库

--- src/test/java/org/spring/springboot/dao/sqlserver/SqlServerReplaceWholeDBDaoTest.java
+++ src/test/java/org/spring/springboot/dao/sqlserver/SqlServerReplaceWholeDBDaoTest.java
@@ -1,5 +1,6 @@
 package org.spring.springboot.dao.sqlserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,7 +16,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -26,6 +29,7 @@ import java.util.List;
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @EnableAutoConfiguration
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@Slf4j
 public class SqlServerReplaceWholeDBDaoTest {
     private List<String> excludeTables = Arrays.asList("By", "flv", "GG", "KKK");
     private List<String> contentFieldTypes = Arrays.asList("varchar", "nvarchar");
@@ -37,18 +41,18 @@ public class SqlServerReplaceWholeDBDaoTest {
     @Test
     public void queryAllTableName() throws Exception {
         List<String> list = sqlServerReplaceWholeDBDao.queryAllTableName();
-        list.forEach(System.out::println);
+        list.forEach(s->log.info(s));
     }
     @Test
     public void queryInfoByTableName() throws Exception {
-        List<TableInfo> list = sqlServerReplaceWholeDBDao.queryInfoByTableName("巡护排班");
+        List<TableInfo> list = sqlServerReplaceWholeDBDao.queryInfoByTableName("sys_DeptInfo");
         list.forEach(System.out::println);
     }
 
     @Test
     public void queryField() throws Exception {
-        List<String> list = sqlServerReplaceWholeDBDao.queryField("巡护排班", "备注", "%"+keyWord+"%");
-        list.forEach(System.out::println);
+        List<String> list = sqlServerReplaceWholeDBDao.queryField("MainMenuConfig", "'Group'", "%%");
+        list.forEach(s->log.info(s));
     }
 
     @Test
@@ -64,9 +68,26 @@ public class SqlServerReplaceWholeDBDaoTest {
             String tableName = tableNameList.get(i);
             List<TableInfo> tableInfoList = sqlServerReplaceWholeDBDao.queryInfoByTableName(tableName);
             for (int j = 0; j < tableInfoList.size(); j++) {
-                String fieldType = tableInfoList.get(j).getData_type();
+                TableInfo info = tableInfoList.get(j);
+                String fieldType = info.getData_type();
+                String fieldName = info.getColumn_name();
+                fieldName = "[" + fieldName + "]";
                 if(contentFieldTypes.contains(fieldType)){
-
+                    List<String> oldFieldValueList = sqlServerReplaceWholeDBDao.queryField(tableName, fieldName, "%"+keyWord+"%");
+                    /**
+                     * oldFieldValueList去重
+                     */
+                    List<String> oldFieldValueList_NoDuplicate = new ArrayList<>(new HashSet<>(oldFieldValueList));
+                    for (int k = 0; k < oldFieldValueList_NoDuplicate.size(); k++) {
+                        String oldFieldValue = oldFieldValueList_NoDuplicate.get(k);
+                        String newFieldValue = oldFieldValue.replace(keyWord, replaceKeyWord);
+                        boolean flag = sqlServerReplaceWholeDBDao.updateField(tableName, fieldName, oldFieldValue, newFieldValue);
+                        if(flag){
+                            log.info("表:" + tableName + "  更新; 字段:" + fieldName + "; 从:" + oldFieldValue + "--->" + newFieldValue);
+                        }else {
+                            log.error("表:" + tableName + "  更新; 字段:" + fieldName + "; 从:" + oldFieldValue + "--->" + newFieldValue);
+                        }
+                    }
                 }
             }
         }
